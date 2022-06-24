6bc2f40ac35759fab6e785e6a1dd42e7e07559c4
unknown <abob15085@gmail.com>
2022-06-24 03:22:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,405 @@
+AOS.init();
+const cont1 =document.querySelector(".cont1");
+const cont2 =document.querySelector(".cont2");
+const cont3 =document.querySelector(".cont3");
+const cont4 =document.querySelector(".cont4");
+const cont5 =document.querySelector(".cont5");
+const cont6 =document.querySelector(".cont6");
+const cont7 =document.querySelector(".cont7");
+const cont8 =document.querySelector(".cont8");
+const cont9 =document.querySelector(".cont9");
+const cont10 =document.querySelector(".cont10");
+const cont11 =document.querySelector(".cont11");
+const cont12 =document.querySelector(".cont12");
+const cont13 =document.querySelector(".cont13");
+const cont14 =document.querySelector(".cont14");
+const cont15 =document.querySelector(".cont15");
+const cont16 =document.querySelector(".cont16");
+
+const btnCont1 = document.querySelector(".btncont1");
+const btncont2 = document.querySelector(".btncont2");
+const btncont3 = document.querySelector(".btncont3");
+const btncont4 = document.querySelector(".btncont4");
+const btncont5 = document.querySelector(".btncont5");
+const btncont6 = document.querySelector(".btncont6");
+const btncont7 = document.querySelector(".btncont7");
+const btncont8 = document.querySelector(".btncont8");
+const btnCont9 = document.querySelector(".btncont9");
+const btncont10 = document.querySelector(".btncont10");
+const btnCont11 = document.querySelector(".btncont11");
+const btnCont12 = document.querySelector(".btncont12");
+const btnCont13 = document.querySelector(".btncont13");
+const btnCont14 = document.querySelector(".btncont14");
+const btnCont15 = document.querySelector(".btncont15");
+const btncont16 = document.querySelector(".btncont16");
+
+const x = document.querySelector(".x");
+
+const DSection = document.querySelector(".DRsection1");
+
+function btn1 () {
+    $(cont1).show();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+
+function btn2 () {
+    $(cont1).hide();
+    $(cont2).show();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+
+function btn3 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).show();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn4 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).show();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn5 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).show();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn6 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).show();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn7 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).show();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn8 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).show();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn9 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).show();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn10 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).show();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn11 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).show();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn12 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).show();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn13 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).show();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn14 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).show();
+    $(cont15).hide();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn15 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).show();
+    $(cont16).hide();
+
+    DSection.classList.add('none')
+};
+function btn16 () {
+    $(cont1).hide();
+    $(cont2).hide();
+    $(cont3).hide();
+    $(cont4).hide();
+    $(cont5).hide();
+    $(cont6).hide();
+    $(cont7).hide();
+    $(cont8).hide();
+    $(cont9).hide();
+    $(cont10).hide();
+    $(cont11).hide();
+    $(cont12).hide();
+    $(cont13).hide();
+    $(cont14).hide();
+    $(cont15).hide();
+    $(cont16).show();
+
+    DSection.classList.add('none')
+};
+function closs () {
+    DSection.classList.remove('none')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(cont1).hide();
+$(cont2).hide();
+$(cont3).hide();
+$(cont4).hide();
+$(cont5).hide();
+$(cont6).hide();
+$(cont7).hide();
+$(cont8).hide();
+$(cont9).hide();
+$(cont10).hide();
+$(cont11).hide();
+$(cont12).hide();
+$(cont13).hide();
+$(cont14).hide();
+$(cont15).hide();
+$(cont16).hide();
